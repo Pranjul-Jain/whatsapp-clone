f27f144702818db041c358cdd4dd1de724d2e86d
Pranjul-Jain <pranjuljainp@gmail.com>
2023-08-09 18:12:14 +0530

    created virtualenvironment

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7(d+1a*0fu&*s$h@lr+__vz!zgd*p$ue)2!0#--d5dm=)+--iw'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
